ce6197c8e3fc79fec69bb05c77ee7fababe3382e
SK <skarode96@gmail.com>
2026-08-05 14:19:17 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,36 +1,59 @@
-import { createInterface } from "readline";
+import {createInterface} from 'readline';
+import fs from 'fs';
+import {access, constants} from 'node:fs/promises';
 
 const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: "$ ",
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '$ ',
 });
 
 rl.prompt();
 
-rl.on("line", (input) => {
-    let command = input.split(" ")[0];
+rl.on('line', async (input) => {
+    let command = input.split(' ')[0];
 
     switch (command) {
-        case "exit": rl.close(); return;
-        case "type": handleType(input); break;
-        case "echo": handleEcho(input); break;
-        default: console.log(`${input}: command not found`);
+        case 'exit':
+            rl.close();
+            return;
+        case 'type':
+            await handleType(input);
+            break;
+        case 'echo':
+            handleEcho(input);
+            break;
+        default:
+            console.log(`${input}: command not found`);
     }
     rl.prompt();
-})
+});
 
-const handleType = (input: string): void => {
-    input = input.replace("type ", "");
-    const types = ["exit", "echo", "type"];
-    if(types.includes(input)) {
-       console.log(`${input} is a shell builtin`);
+const handleType = async (input: string): Promise<void> => {
+    input = input.replace('type ', '');
+    const types = ['exit', 'echo', 'type'];
+    const paths: string[] = process.env.PATH!.split(':');
+    if (types.includes(input)) {
+        console.log(`${input} is a shell builtin`);
+        return;
     } else {
-        console.log(`${input}: not found`);
+        for (const path of paths) {
+            let execPath = path + '/' + input;
+            if (fs.existsSync(execPath)) {
+                try {
+                    await access(execPath, fs.constants.X_OK);
+                    console.log(`${input} is ${execPath}`);
+                    return;
+                } catch (e) {
+
+                }
+            }
+        }
     }
-}
+    console.log(`${input}: not found`);
+};
 
 const handleEcho = (input: string): void => {
-    input = input.replace("echo ", "");
+    input = input.replace('echo ', '');
     console.log(input);
-}
+};
