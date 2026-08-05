d9a012cf60a650d65dbb0dccb9398cc381f0f40f
SK <skarode96@gmail.com>
2026-08-05 11:07:26 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -10,15 +10,23 @@ rl.prompt();
 
 rl.on("line", (input) => {
     let command = input.split(" ")[0];
+
     switch (command) {
         case "exit": rl.close(); return;
+        case "type": handleType(input); break;
         case "echo": handleEcho(input); break;
         default: console.log(`${input}: command not found`);
     }
     rl.prompt();
 })
 
-const handleEcho = (input: string)=> {
+const handleType = (input: string): void => {
+    input = input.replace("type ", "");
+    const types = ["exit", "echo", "type"];
+    types.includes(input) ? console.log(`${input} is a shell builtin`) : console.log(`${input}: not found`);
+}
+
+const handleEcho = (input: string): void => {
     input = input.replace("echo ", "");
     console.log(input);
 }
